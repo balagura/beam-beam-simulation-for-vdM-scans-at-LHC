97555de1ab9b38116c825c628ef44e6e0fc36ce4
Vladislav BALAGURA <balagura@cern.ch>
2019-05-22 17:38:10 +0200

    make simulation for N transitional turns = 1 and 1000

--- compare_with_dynamic_beta_and_deflection.sh
+++ compare_with_dynamic_beta_and_deflection.sh
@@ -8,34 +8,33 @@ filename="${file%%.*}"
 kicks="precise precise.minus.average average precise.minus.one.third.of.average "\
 "one.third.of.average precise.minus.at.center at.center quadrupole average.and.quadrupole"
 
+n_transional_turns="1 1000"
+
 for kick in ${kicks}
 do
-    # add _kick prefix except for precise
-    if [ "${kick}" == "precise" ]
-    then
-	dir=${filename}
-    else
-	dir=${filename}_${kick}
-    fi
-    f=${dir}.${extension}
-    # create not git-controlled *_kick.txt files if they do not exist
-    if [ ! -f "${f}" ]
-    then
-	# substitute eg. kick=precise -> kick=const etc.
-	sed "/^[ \t]*kick.model[ \t]*=[ \t]*precise[ \t]*$/ s/precise/${kick}/" ${file} > ${f}
-    fi
-    echo "========================================"
-    echo "        Simulate ${kick}"
-    echo "========================================"
-    make ${dir}/summary.txt
-    cd comparison_with_dynamic_beta_and_deflection_in_R
-    make ../${dir}/pdf/gg.cor.pdf
-    # show final plots
-    xdg-open ../${dir}/pdf/gg.cor.pdf &
-    if [[ ${kick} == *precise.minus.* ]]; then
-	make ../${dir}/pdf/gg.cor.dipole.subtr.and.added.pdf
+    for ntrans in ${n_transional_turns}
+    do
+	dir=${filename}_${kick}_${ntrans}
+	f=${dir}.${extension}
+	# substitute
+	# kick=precise -> kick=const 
+	# N.transitional.turns=1000 -> N.transitional.turns=1 etc.
+	sed -e "/^[ \t]*kick.model[ \t]*=[ \t]*precise[ \t]*$/ s/precise/${kick}/" \
+	    -e "/^[ \t]*N.transitional.turns[ \t]*=[ \t]*1000[ \t]*$/ s/1000/${ntrans}/" \
+	    ${file} > ${f}
+	echo "======================================================================"
+	echo "        Simulate ${kick} with ${ntrans} transitional turns"
+	echo "======================================================================"
+	make ${dir}/summary.txt
+	cd comparison_with_dynamic_beta_and_deflection_in_R
+	make ../${dir}/pdf/gg.cor.pdf
 	# show final plots
-	xdg-open ../${dir}/pdf/gg.cor.dipole.subtr.and.added.pdf &
-    fi
-    cd ..
+	xdg-open ../${dir}/pdf/gg.cor.pdf &
+	if [[ ${kick} == *precise.minus.* ]]; then
+	    make ../${dir}/pdf/gg.cor.dipole.subtr.and.added.pdf
+	    # show final plots
+	    xdg-open ../${dir}/pdf/gg.cor.dipole.subtr.and.added.pdf &
+	fi
+	cd ..
+    done
 done
